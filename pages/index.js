c1db12ad77475b1d7bc759f45f8124103dc3b7fc
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-02 13:42:15 +0100

    feat: tailwind: clearing the CSS

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,6 @@ export default function Home() {
 
       <main>
         <Header title="Welcome to the Ontrack Customer Portal!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
