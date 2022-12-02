5d0758979361b250e37d9ca54a9c40e33a3313ba
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-02 14:26:00 +0100

    feat: tailwind: footer padding

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <>
-            <footer className="flex flex-row gap-2 justify-center">
+            <footer className="flex flex-row gap-2 justify-center pt-10">
                 <img src="/ontrack-128.png" alt="Ontrack Logo" className="w-6 h-6"/>
                 <a href="https://nemerosa.github.io/ontrack/">Ontrack © 2022-2023</a>
             </footer>
