7b117efbc74d14ea4c80a1731883501abc6c23dd
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-02 14:24:07 +0100

    feat: tailwind: styling the title

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
-export default function Header({ title }) {
-  return <h1>{title}</h1>
+export default function Header({title}) {
+    return <h1 className="flex items-center max-w-lg mx-auto text-xl font-bold">{title}</h1>
 }
--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import Header from '@components/Header'
 import Footer from '@components/Footer'
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Ontrack Customer Portal</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-      </Head>
+    return (
+        <div className="container">
+            <Head>
+                <title>Ontrack Customer Portal</title>
+                <link rel="icon" href="/favicon.ico"/>
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+            </Head>
 
-      <main>
-        <Header title="Welcome to the Ontrack Customer Portal!" />
-      </main>
+            <main className="container">
+                <Header title="Welcome to the Ontrack Customer Portal!"/>
+            </main>
 
-      <Footer />
-    </div>
-  )
+            <Footer/>
+        </div>
+    )
 }
