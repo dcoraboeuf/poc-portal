b6cd125014d854981df94fdbece0d717c63dd377
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-02 13:57:03 +0100

    feat: tailwind: icon size

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
             <footer>
-                <img src="/ontrack-128.png" alt="Ontrack Logo"/>
+                <img src="/ontrack-128.png" alt="Ontrack Logo" className="w-6 h-6"/>
                 <a href="https://nemerosa.github.io/ontrack/">Ontrack</a>
             </footer>
         </>
