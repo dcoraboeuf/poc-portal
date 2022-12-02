422ce500692bdc7e1c87da3f44e5aa6e50fc96ee
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-02 13:46:58 +0100

    feat: tailwind: clearing the CSS

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,8 @@
-import styles from './Footer.module.css'
-
 export default function Footer() {
     return (
         <>
-            <footer className={styles.footer}>
-                <img src="/ontrack-128.png" alt="Ontrack Logo" className={styles.logo}/>
+            <footer>
+                <img src="/ontrack-128.png" alt="Ontrack Logo"/>
                 <a href="https://nemerosa.github.io/ontrack/">Ontrack</a>
             </footer>
         </>
