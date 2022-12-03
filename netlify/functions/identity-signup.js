21e536c0c432bf42a9f1952006ecf064dee75ea5
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-03 09:37:08 +0100

    chore: `identity-signup` function must return a result

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,4 +1,14 @@
 exports.handler = async (event) => {
     const { user } = JSON.parse(event.body);
     console.log("user = ", user);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            // No role
+            // app_metadata: {
+            //     roles: ['free'],
+            // },
+        }),
+    };
 };
