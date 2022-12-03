93bc141713f8332584b40ec1a76cd60a15ad079c
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-03 17:44:06 +0100

    chore: basic styling for the input

--- pages/subscriptions/creation.js
+++ pages/subscriptions/creation.js
@@ -44,7 +44,23 @@ export default function SubscriptionCreation() {
             <form method="post">
                 <label>
                     Instance name:
-                    <input type="text" required={true}/>
+                    <input type="text" required={true} className="
+                        form-control
+                        block
+                        w-100
+                        px-3
+                        py-1.5
+                        text-base
+                        font-normal
+                        bg-white bg-clip-padding
+                        border border-solid border-gray-300
+                        rounded
+                        transition
+                        ease-in-out
+                        m-0
+                        my-2
+                        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                        "/>
                 </label>
             </form>
 
