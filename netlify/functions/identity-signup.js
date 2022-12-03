4a3d9dc35900594fc29eeb79ea74638550241807
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-03 09:48:09 +0100

    chore: registering the customer in Stripe

--- netlify/functions/identity-signup.js
+++ netlify/functions/identity-signup.js
@@ -1,7 +1,19 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+/**
+ * This hook is called upon the registration of a new customer in the portal.
+ */
 exports.handler = async (event) => {
     const {user} = JSON.parse(event.body);
-    console.log("user = ", user);
 
+    // Creates a new customer in Stripe
+    const customer = await stripe.customers.create({name: user.user_metadata.full_name, email: user.email});
+    console.log({
+        user: user,
+        customer: customer,
+    });
+
+    // Assign a role to the identity in Netlify
     return {
         statusCode: 200,
         body: JSON.stringify({
