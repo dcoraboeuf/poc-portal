e876a668722b5fe7f26641481beeb862f29eda22
Damien Coraboeuf <damien.coraboeuf@collibra.com>
2022-12-07 10:57:48 +0100

    chore(bootstrap): navigation bar

--- components/MainLayout.js
+++ components/MainLayout.js
@@ -9,11 +9,44 @@ export default function MainLayout({children}) {
                     <title>Ontrack Customer Portal</title>
                 </Head>
 
-                <main>
-                    {children}
-                </main>
+                <nav className="navbar bg-dark">
+                    <div className="container">
+                        <a className="navbar-brand" href="/">
+                            <img src="/ontrack-128.png" alt="Ontrack logo" className="ot-icon"/>
+                            <span className="ps-4 text-light">Ontrack Customer Portal</span>
+                        </a>
+                        <form className="d-flex" role="connection">
+                            <span className="navbar-text text-light text-muted pe-4">
+                              Customer name
+                            </span>
+                            <button type="button" className="btn btn-outline-light me-2">Login</button>
+                            <button type="button" className="btn btn-warning">Sign-up</button>
+                        </form>
+                    </div>
+                </nav>
 
-                <Footer/>
+                {/*<div className="container">*/}
+                {/*    <header className="p-3 text-bg-dark">*/}
+                {/*        <div*/}
+                {/*            className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">*/}
+                {/*            <a href="/"*/}
+                {/*               className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">*/}
+                {/*                <img src="/ontrack-128.png" alt="Ontrack Logo" className="ot-icon"/>*/}
+                {/*                <span className="fs-4 ps-4">Ontrack Customer Portal</span>*/}
+                {/*            </a>*/}
+                {/*            <div className="text-end">*/}
+                {/*                <button type="button" className="btn btn-outline-light me-2">Login</button>*/}
+                {/*                <button type="button" className="btn btn-warning">Sign-up</button>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </header>*/}
+                {/*</div>*/}
+
+                {/*<main>*/}
+                {/*    {children}*/}
+                {/*</main>*/}
+
+                {/*<Footer/>*/}
             </div>
         </>
     )
